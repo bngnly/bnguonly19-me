db4c44a2977f8aa2aec178c4d56119c45bc1ad27
bngnly <bnguonly19@gmail.com>
2025-06-13 23:59:00 -0700

    Default map page to use the latest thailand trip

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -5,7 +5,7 @@ import { Photo } from "@/types/types";
 export const dynamic = "force-dynamic";
 
 export default async function MapPage() {
-  const photos: Photo[] = await getAlbumPhotos("here");
+  const photos: Photo[] = await getAlbumPhotos("2025_01_18_Thailand_v2");
 
   return <MapWrapper photos={photos} />;
 }
