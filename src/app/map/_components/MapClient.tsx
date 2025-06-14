1ef2da05a3604bfd2e18f1564be940b5ed3eb32a
bngnly <bnguonly19@gmail.com>
2025-06-13 22:28:34 -0700

    Restrict the map size to only one globe

--- src/app/map/_components/MapClient.tsx
+++ src/app/map/_components/MapClient.tsx
@@ -18,14 +18,23 @@ export default function MapClient({ photos }: MapClientProps) {
   return (
     <MapContainer
       center={[34.0614, -118.308]} // Koreatown, LA
-      zoom={14}
+      zoom={10}
+      minZoom={4}
+      maxZoom={18}
       scrollWheelZoom={true}
       zoomControl={false}
       className="w-full h-full"
+      worldCopyJump={false}
+      maxBounds={[
+        [-90, -180],
+        [90, 180],
+      ]}
+      maxBoundsViscosity={1.0}
     >
       <TileLayer
         url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+        noWrap={true}
       />
       {validPhotos.map((photo) => (
         <PhotoMarker key={photo.key} photo={photo} />
